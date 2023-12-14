b484922ca3ad11dcaf48c19565a92e89351e68a9
TheChesireCat <aliceoxenbury@gmail.com>
2023-12-13 19:15:02 -0500

    rearrange for prod 7 : Saath Khoon Maaf

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,14 +1,6 @@
-// import mysql from 'mysql2/promise';
 
 import postgres from 'postgres'
 
-// const sql = postgres({ 
-//   host: process.env.POSTGRES_HOST,
-//   port: process.env.POSTGRES_PORT,
-//   database: process.env.POSTGRES_DB,
-//   user: process.env.POSTGRES_USER,
-//   onnotice: notice => {return notice;}
-//  }) 
 
 const connectionString = process.env.SUPA_CONNECTION_STRING
 const sql = postgres(connectionString)
@@ -16,18 +8,3 @@ const sql = postgres(connectionString)
 export default sql;
 
 
-// export async function executeQuery({ query, values }) {
-//   try {
-//     const db = await mysql.createConnection({
-//       host: process.env.DATABASE_HOST,
-//       user: process.env.DATABASE_USER,
-//       password: process.env.DATABASE_PASSWORD,
-//       database: process.env.DATABASE_NAME,
-//     });
-//     const results = await db.query(query, values);
-//     await db.end();
-//     return results;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
